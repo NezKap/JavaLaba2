6ef92e34871d492a07b1247a005e51df861f4a22
NezKap <werras206@mail.ru>
2025-11-15 20:59:40 +0300

    Создан класс Solver и класс для тестирования будущих методов

---
+++ src/test/java/org/example/Laba2Test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Laba2Test {
+    @Test
+    void testMethod() {
+
+    }
+}
